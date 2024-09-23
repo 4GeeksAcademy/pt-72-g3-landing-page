4e2fe62b3cfc81c003830ad3e5a213871246f25f
Fermin Rodriguez II <rdrgzfermin@gmail.com>
2024-09-23 23:28:37 +0000

    Cards Done

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,43 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
 //create your first component
-const Home = () => {
+const Cards = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div class="card-container">
+			<div class="card" style="width: 18rem;">
+				<img src="https://picsum.photos/id/237/200/300" class="card-img-top" alt="..."/>
+				<div class="card-body">
+					<h5 class="card-title">Dogs Needing Homes</h5>
+					<p class="card-text">Lorem ipsum odor amet, consectetuer adipiscing elit. Ultrices ridiculus tempus risus pellentesque penatibus potenti. Justo parturient maecenas luctus aenean auctor laoreet. Nulla placerat suscipit auctor mauris parturient nostra ex nibh egestas.</p>
+					<a href="#" class="btn btn-primary">Find Out More</a>
+				</div>
+			</div>
+			<div class="card" style="width: 18rem;">
+				<img src="https://picsum.photos/id/100/200/300" class="card-img-top" alt="..."/>
+				<div class="card-body">
+					<h5 class="card-title">Beaches Of The World</h5>
+					<p class="card-text">Lorem ipsum odor amet, consectetuer adipiscing elit. Id nostra etiam convallis est hac fames suscipit. Aliquam quam bibendum consectetur luctus tempor lobortis lorem. Vivamus nibh varius bibendum primis lorem egestas pulvinar nisi.</p>
+					<a href="#" class="btn btn-primary">Find Out More</a>
+				</div>
+			</div>
+			<div class="card" style="width: 18rem;">
+				<img src="https://picsum.photos/id/111/200/300" class="card-img-top" alt="..."/>
+				<div class="card-body">
+					<h5 class="card-title">Classic Cars</h5>
+					<p class="card-text">Lorem ipsum odor amet, consectetuer adipiscing elit. Id orci vehicula finibus ad at integer. Hendrerit eleifend proin hac pellentesque non per interdum tempor amet.</p>
+					<a href="#" class="btn btn-primary">Find Out More</a>
+				</div>
+			</div>
+			<div class="card" style="width: 18rem;">
+				<img src="https://picsum.photos/id/142/200/300" class="card-img-top" alt="..."/>
+				<div class="card-body">
+					<h5 class="card-title">Castles and Estates of the World</h5>
+					<p class="card-text">Lorem ipsum odor amet, consectetuer adipiscing elit. Himenaeos conubia sagittis; turpis natoque mi mollis.</p>
+					<a href="#" class="btn btn-primary">Find Out More</a>
+				</div>
+			</div>
 		</div>
 	);
 };
 
-export default Home;
+export default Cards;
