d99a8cf21e6943429de90bfa6bcf5a5a4282fc0b
Joaquin Morales <Mintinvestments95@gmail.com>
2024-09-24 00:16:59 +0000

    completed JumboTron

---
+++ src/js/component/JumboTron.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <div className="container mt-5">
+            <div className="jumbotron text-start bg-light p-5 rounded">
+                <h1 className="display-4">
+                    A Warm Welcome!
+                </h1>
+                <p className="lead">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi iusto
+                    nihil ad molestiae deserunt repellendus consequuntur quos delectus qui
+                    aliquid, voluptatum suscipit laboriosam beatae optio modi voluptatem
+                    fugiat, eligendi quis.
+                </p>
+                <a href="#" className="btn btn-primary btn-lg">
+                    Call to action!
+                </a>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,22 +1,10 @@
 import React from "react";
+import JumboTron from "./JumboTron";
 
 const Home = () => {
 	return (
-		<div className="container mt-5">
-			<div className="jumbotron text-left bg-light p-5 rounded">
-				<h1 className="display-4">
-					A Warm Welcome!
-				</h1>
-				<p className="lead">
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi iusto
-					nihil ad molestiae deserunt repellendus consequuntur quos delectus qui
-					aliquid, voluptatum suscipit laboriosam beatae optio modi voluptatem
-					fugiat, eligendi quis.
-				</p>
-				<a href="#" className="btn btn-primary btn-lg">
-					Call to action!
-				</a>
-			</div>
+		<div className="text-center">
+			<JumboTron />
 		</div>
 	);
 };
